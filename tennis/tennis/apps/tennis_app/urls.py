2f6d1cbfb068b966bacc02001e95bb2a1cee1f30
RayRaf <rayraf@mail.ru>
2025-03-24 19:17:59 +0500

    Добавил стили и главную страницу

--- tennis/tennis/apps/tennis_app/urls.py
+++ tennis/tennis/apps/tennis_app/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'tennis_app'
 
 urlpatterns = [
-    path('', views.main, name='main')
-]
+    path('', views.main, name='main'),
+    path('free_play', views.play, name='free_play'),
+    path('my_tournaments', views.my_tournaments, name='my_tournaments'),
+    path('rules', views.rules, name='rules'),
+    ]
--- tennis/tennis/apps/tennis_app/views.py
+++ tennis/tennis/apps/tennis_app/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def main(request):
     return render(request, 'home_index.html')
+
+def play(request):
+    return render(request, 'play.html')
+
+def my_tournaments(request):
+    return render(request, 'my_tournaments.html')
+
+def rules(request):
+    return render(request, 'rules.html')
+
